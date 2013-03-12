26a479d81a66491567964928fdb7617b0b8891ed
Duy <coldzero1120@gmail.com>
2013-03-12 11:56:55 +0700

    mailboxer engine to mongoid

--- lib/mailboxer/engine.rb
+++ lib/mailboxer/engine.rb
@@ -10,7 +10,7 @@ module Mailboxer
   class Engine < Rails::Engine
     
     initializer "mailboxer.models.messageable" do
-      ActiveSupport.on_load(:active_record) do
+      ActiveSupport.on_load(:mongoid) do
         include Mailboxer::Models::Messageable
       end
     end
